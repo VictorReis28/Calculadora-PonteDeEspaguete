89729946adbcc61f4b8380e3f76fe4ac8461e299
Victor Reis Furtado <90731549+VictorReis28@users.noreply.github.com>
2024-05-11 21:28:07 -0300

    models

--- ponte/models.py
+++ ponte/models.py
@@ -1,23 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+tipos = {'T': 'Tração', 'C': 'Compressão'}
 class Ponte(models.Model):
-    nome = models.CharField(max_length=50)
-    peso_linear = models.FloatField()
-    peso_metade = models.FloatField()
+    models.ForeignKey(User, on_delete=models.CASCADE)
+    nome_ponte = models.CharField(max_length=50)
+    peso_linear = models.FloatField(null=True)
+    peso_metade = models.FloatField(null=True)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover = models.ImageField(upload_to='ponte/covers/%Y/%m/%d/', default='', blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.nome_ponte
 
 class Barra(models.Model):
     ponte = models.ForeignKey(Ponte, on_delete=models.CASCADE)
     nome = models.CharField(max_length=50)
-    cm = models.FloatField()
-    esforco_interno = models.FloatField()
-    tipo = models.CharField(max_length=5)
-    n_fios = models.FloatField()
-    n_fios_revisados = models.FloatField()
+    cm = models.FloatField(null=True)
+    esforco_interno = models.FloatField(null=True)
+    tipo = models.CharField(max_length=1, choices=tipos)
+    n_fios = models.FloatField(null=True)
+    n_fios_revisados = models.FloatField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'Barras'
