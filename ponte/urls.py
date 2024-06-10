4f4e99201dd30a3d0846ac713f48e463696a2abd
Victor Reis Furtado <90731549+VictorReis28@users.noreply.github.com>
2024-06-10 00:32:46 -0300

    Botão excluir funcionando

--- ponte/urls.py
+++ ponte/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from ponte.views import index
 from ponte.views import ponte_detail
+from ponte.views import excluir_barra
 
 urlpatterns = [
     path('', index, name='index'),
     path('ponte/<str:username>/<int:ponte_id>/', ponte_detail, name='ponte_detail'),
+    path('ponte/<str:username>/<int:ponte_id>/excluir_barra/<int:barra_id>/', excluir_barra, name='excluir_barra'),
 ]
--- ponte/views.py
+++ ponte/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Ponte, Barra
 
 # Create your views here.
@@ -12,3 +12,8 @@ def ponte_detail(request, username, ponte_id):
     ponte = get_object_or_404(Ponte, id=ponte_id, autor__username=username)
     barras = Barra.objects.filter(ponte=ponte)
     return render(request, 'ponte/pages/resultados.html', {'ponte': ponte, 'barras': barras})
+
+def excluir_barra(request, username, ponte_id, barra_id):
+    barra = get_object_or_404(Barra, id=barra_id, ponte__id=ponte_id, ponte__autor__username=username)
+    barra.delete()
+    return redirect('ponte_detail', username=username, ponte_id=ponte_id)
